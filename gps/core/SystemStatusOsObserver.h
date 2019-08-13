b8a291a8f2f693675c6076b7eb74f78f54df44c1
ricksanchez777 <henripaz1998@gmail.com>
2019-08-13 20:18:33 +0000

    Revert "platina: Update GPS from LA.UM.7.2.r1-04500-sdm660.0"
    This reverts commit 6029c84f3f24febae0e3b441b706eadb4ee76147.
    Signed-off-by: Rique Barros <henripaz1998@gmail.com>

--- gps/core/SystemStatusOsObserver.h
+++ gps/core/SystemStatusOsObserver.h
@@ -39,8 +39,7 @@
 #include <MsgTask.h>
 #include <DataItemId.h>
 #include <IOsObserver.h>
-#include <loc_pla.h>
-#include <log_util.h>
+#include <platform_lib_log_util.h>
 #include <LocUnorderedSetMap.h>
 
 namespace loc_core
@@ -106,12 +105,6 @@ public:
     // To set the framework action request object
     inline void setFrameworkActionReqObj(IFrameworkActionReq* frameworkActionReqObj) {
         mContext.mFrameworkActionReqObj = frameworkActionReqObj;
-#ifdef USE_GLIB
-        if (mBackHaulConnectReqCount > 0) {
-            connectBackhaul();
-            mBackHaulConnectReqCount = 0;
-        }
-#endif
     }
 
     // IDataItemSubscription Overrides
@@ -134,10 +127,6 @@ public:
     // IFrameworkActionReq Overrides
     virtual void turnOn(DataItemId dit, int timeOut = 0) override;
     virtual void turnOff(DataItemId dit) override;
-#ifdef USE_GLIB
-    virtual bool connectBackhaul() override;
-    virtual bool disconnectBackhaul();
-#endif
 
 private:
     SystemStatus*                                    mSystemStatus;
@@ -151,10 +140,6 @@ private:
     // Cache the subscribe and requestData till subscription obj is obtained
     void cacheObserverRequest(ObserverReqCache& reqCache,
             const list<DataItemId>& l, IDataItemObserver* client);
-#ifdef USE_GLIB
-    // Cache the framework action request for connect/disconnect
-    int         mBackHaulConnectReqCount;
-#endif
 
     void subscribe(const list<DataItemId>& l, IDataItemObserver* client, bool toRequestData);
 
--- gps/utils/platform_lib_abstractions/loc_stub/src/loc_stub_android_runtime.cpp
+++ gps/utils/platform_lib_abstractions/loc_stub/src/loc_stub_android_runtime.cpp
@@ -26,42 +26,16 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef __LOC_PLA__
-#define __LOC_PLA__
+#include "loc_stub_android_runtime.h"
 
-#ifdef __cplusplus
-#include <utils/SystemClock.h>
-#define uptimeMillis android::uptimeMillis
+namespace android {
 
-extern "C" {
-#endif
-
-#include <cutils/properties.h>
-#include <cutils/threads.h>
-#include <cutils/sched_policy.h>
-#include <sys/time.h>
-#include <string.h>
-#include <stdlib.h>
-#ifndef OFF_TARGET
-#include <glib.h>
-#define strlcat g_strlcat
-#define strlcpy g_strlcpy
-#else
-#define strlcat strncat
-#define strlcpy strncpy
-#endif
-
-#define LOC_PATH_GPS_CONF_STR      "/etc/gps.conf"
-#define LOC_PATH_IZAT_CONF_STR     "/etc/izat.conf"
-#define LOC_PATH_FLP_CONF_STR      "/etc/flp.conf"
-#define LOC_PATH_LOWI_CONF_STR     "/etc/lowi.conf"
-#define LOC_PATH_SAP_CONF_STR      "/etc/sap.conf"
-#define LOC_PATH_APDR_CONF_STR     "/etc/apdr.conf"
-#define LOC_PATH_XTWIFI_CONF_STR   "/etc/xtwifi.conf"
-#define LOC_PATH_QUIPC_CONF_STR    "/etc/quipc.conf"
-
-#ifdef __cplusplus
+pthread_t AndroidRuntime::createJavaThread(const char* /*name*/,
+    void (*start)(void *), void* arg)
+{
+    pthread_t threadId = 0;
+    pthread_create(&threadId, NULL, (void *(*)(void*))start, arg);
+    return threadId;
 }
-#endif /*__cplusplus */
 
-#endif /* __LOC_PLA__ */
+}
